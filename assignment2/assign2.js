b9a05062d8922de23071e06a236f068f9924e600
girish AR <giri754@gmail.com>
2019-04-04 23:42:21 +0530

    Header added in assignment2

--- assignment2/assign2.js
+++ assignment2/assign2.js
@@ -4,55 +4,49 @@ var btn = document.querySelector(".btn_submit");
 	btn.addEventListener("click", function()
 	 {
 		const xhr = new XMLHttpRequest();
-		xhr.open('get','dsafsadfasdads.txt',false);
+		xhr.open('get','filmslist.json',false);
+
 		
 
-		xhr.onreadystatechange = function ()
+	xhr.onreadystatechange = function ()
 		 {
 			if (xhr.readyState == 4) 
 			{
 				if (xhr.status == 200) 
 				{
 					var data = xhr.responseText;
-	                var jsonResponse = JSON.parse(data);
-	                console.log(jsonResponse["Row"]);
-	                var table = document.createElement('table');
-	                table.setAttribute('class', 'history');
-	                var properties = ['Title', 'Year', 'Hubs', 'Gross', 'HomePage','Logo']; // changed this
-	                var capitalize = function(s) 
-	                {
-	                    return s.charAt(0).toUpperCase() + s.slice(1);
-	                }
-	                var tr = document.createElement('tr');
-	                for (var i = 0; i < properties.length; i++) 
-	                {
-	                    var th = document.createElement('th');
-	                    th.appendChild(document.createTextNode(capitalize(properties[i])));
-	                    tr.appendChild(th);
-	                }
-	                table.appendChild(tr);
-	                var tr, row;
-	                console.log("jsonResponse", jsonResponse); // changed this
-	                for (var r = 0; r < jsonResponse["presidents"].president.length; r++)
-	                 { // changed this
-	                    tr = document.createElement('tr');
-	                    row = jsonResponse["presidents"].president[r]; // changed this
-	                    for (var i = 0; i < properties.length; i++) 
-	                    {
-	                        var td = document.createElement('td');
-	                        td.appendChild(document.createTextNode(row[properties[i]]));
-	                        tr.appendChild(td);
-	                    }
-	                    table.appendChild(tr);
-	                }
-	                document.getElementById('presidentialTable').appendChild(table);
-    		    }
-			}
+	                var tableInfo = JSON.parse(data);
 
-				if (xhr.status == 400) {
+	                var table = document.createElement("table");
+					table.style.width = '50%';
+					table.setAttribute('border', '1');
+					// table.setAttribute('cellspacing', '0');
+					// table.setAttribute('cellpadding', '5');
+
+					var tHead = document.createElement("tHead");
+					var hRow = document.createElement("tr");
+					var col = tableInfo.Mainline.Table.Header.Data.length;	
+				
+					for (var i = 0; i < col; i++) {
+						var th = document.createElement("th");
+						console.log(tableInfo.Mainline.Table.Header.Data[i]);
+						th.innerHTML = tableInfo.Mainline.Table.Header.Data[i];
+						hRow.appendChild(th);
+					}
+					tHead.appendChild(hRow);
+					table.appendChild(tHead);
+
+					var divContainer = document.getElementById("filmListTable");
+					divContainer.innerHTML = "";
+					divContainer.appendChild(table);
+    		    }
+    		    if (xhr.status == 400) {
 					console.log("File or Not Found")
 				}
-			}	
+			}		
+		}	
+
+	xhr.open("get","filmslist.json",false);
 
 	xhr.send();	
 
